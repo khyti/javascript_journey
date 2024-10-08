97812c3990c9dfd83a8274363d8acf6c75c9ae62
khyti <121668295+khyti@users.noreply.github.com>
2024-10-08 10:50:20 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "33";
+console.log(typeof score);
+console.log(typeof(score));
+let string = Number(score)
+console.log(typeof string);
+let score1 = "33abc"
+console.log(Number(score1));
+
+
+let value = null
+console.log(Number(value));
+let val = undefined
+console.log(Number(val));
+
+//"33"=>33
+//"33abc"=>NaN
+//true = 1 false =0
+
+
+let isLogged = 1
+console.log(Boolean(isLogged));
+
+// 1 => true 0 => false ""=>false "khyati"=> true
+
+let someValue = 33
+console.log(String(someValue))
+
+
+
