aaf0b38ba3c69cf8e992b63899f39dea76372f4e
khyti <121668295+khyti@users.noreply.github.com>
2024-10-08 11:48:12 +0000

    Datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+//primitive
+
+//String,Number,Boolean,undefined,null,BigInt
+
+const score = 100
+const scoreValue = 1000.3
+let constantValue;
+
+let bigNum = 23334444785875858585905469n;
+const IsLoGGEDiN =true;
+
+const temp = null
+
+const id = Symbol("1234")
+const anotherId = Symbol("1234")
+
+console.log(id == anotherId);
+
+
+//Reference(Non - primitive)
+//Arrays,Objects,Functions
+
+
+const heroes = ["shaktiman","naagraj", "doga"]
+let myObj = {
+    name : "khyati",
+    age :  22,
+
+}
+
+const myFunction = function(){
+    console.log("HelloWorld")
+}
+console.log(typeof myFunction)
+console.log(typeof heroes)
