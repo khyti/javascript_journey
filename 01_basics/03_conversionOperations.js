ce133735e171592dc445e24952cfe1e26ba23d42
khyti <121668295+khyti@users.noreply.github.com>
2024-10-08 11:07:50 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -25,5 +25,42 @@ console.log(Boolean(isLogged));
 let someValue = 33
 console.log(String(someValue))
 
+/*********************************Operations******************************** */
+
+
+
+let value1 = 3
+let negValue = -value1
+console.log(negValue);
+
+
+console.log(2+2)
+console.log(2-2);
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+
+let str1 = "hello"
+let str2 = " Khyati"
+str3 = str1 + str2
+console.log(str3);
+
+console.log("2"+"1")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(2+1+"2")
+console.log(true)
+console.log(+true)
+console.log(+"")
+
+
+let num1,num2,num3
+num1 = num2 =num3 = 2+2
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
+
+
 
 
