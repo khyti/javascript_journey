49d15373e2f482fb234bedc1249e4102f157f5fa
khyti <121668295+khyti@users.noreply.github.com>
2024-10-09 12:03:54 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "khyati"
+const num = 23
+console.log(name + num);
+  
+console.log(`Hello my name is ${name} and my number is ${num}`);
+const game = new String("Khyati");
+console.log(game.length);
+console.log(game.toLowerCase());
+console.log(game.__proto__)
+console.log(game.toUpperCase);
+console.log(game.charAt(3))
+console.log(game.indexOf('t'));
+ const newString = game.substring(0,4)
+ console.log(newString);
+console.log(game.slice(-1,4))
+
+const str=new String("     khyati    ")
+console.log(str.trim());
+
+const url ="https://khyati.com//khyati%20jha"
+console.log(url.replace("%20","-"));
+console.log(url.includes("sundar"));
+console.log(url.split("-"))
+
+
+ 
+
+
