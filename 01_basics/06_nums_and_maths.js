b247575f88c6642995ecfd87e3bf7a25e0844760
khyti <121668295+khyti@users.noreply.github.com>
2024-10-09 12:36:00 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,41 @@
+const score = 23
+console.log(score);
+const num = new Number(400)
+console.log(num)
+
+
+
+console.log(num.toString().length);
+
+console.log(num.toFixed(2))
+
+const otherNumber = 1123.865
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000000
+console.log(hundreds.toLocaleString("en-In"));
+
+
+//++++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++++++++++++\
+
+console.log(Math);
+console.log(Math.abs(-8));
+console.log(Math.round(4.77))
+console.log(Math.ceil(4.78));
+console.log(Math.min(1,3,5,0));
+console.log(Math.max(2,9,11,0));
+console.log(Math.floor(3.8))
+console.log(Math.random())
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10) +1)
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max-min +1))+min)
+
+
+
+
+
+
+
